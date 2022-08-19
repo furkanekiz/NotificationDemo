5596aec2fb0c9d393f21f383ab67a5fbac66dea5
furkanekiz <furkanekiz.06@gmail.com>
2022-08-19 17:17:21 +0300

    Tab Action added

--- app/src/main/java/com/furkanekiz/notificationdemo/ACMain.kt
+++ app/src/main/java/com/furkanekiz/notificationdemo/ACMain.kt
@@ -1,49 +1,60 @@
 package com.furkanekiz.notificationdemo
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.ClipDescription
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
-import androidx.core.graphics.drawable.IconCompat
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.ac_main.*
 
-class MainActivity : AppCompatActivity() {
+class ACMain : AppCompatActivity() {
 
     private val channelID = "com.furkanekiz.notificationdemo.channel1"
     private var notificationManager: NotificationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ac_main)
 
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        createNotificationChannel(channelID,"DemoChannel","This is a demo")
+        createNotificationChannel(channelID, "DemoChannel", "This is a demo")
 
         btn.setOnClickListener {
             displayNotification()
         }
     }
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     private fun displayNotification() {
         val notificationId = 45
-        val notification = NotificationCompat.Builder(this,channelID)
+        val tapResultIntent = Intent(this, ACSecond::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = NotificationCompat.Builder(this, channelID)
             .setContentTitle("Demo Title")
             .setContentText("This is a demo notification")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
-        notificationManager?.notify(notificationId,notification)
+        notificationManager?.notify(notificationId, notification)
     }
 
     private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(id,name,importance).apply {
+            val channel = NotificationChannel(id, name, importance).apply {
                 description = channelDescription
             }
             notificationManager?.createNotificationChannel(channel)
---
+++ app/src/main/java/com/furkanekiz/notificationdemo/ACSecond.kt
@@ -0,0 +1,11 @@
+package com.furkanekiz.notificationdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACSecond : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ac_second)
+    }
+}
